7a71524c955f40ff1f88a6187ca7475acca497b1
ztaaso <ztaaso.cedaspy@gmail.com>
2021-03-30 18:57:29 -0300

    show error when exception

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -25,20 +25,26 @@ class ClassesController {
 
     const timeInMinutes = convertHoursToMinutes(time);
 
-    const classes = await db('classes')
-      .whereExists(function () {
-        this.select('class_schedule.*')
-          .from('class_schedule')
-          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
-      })
-      .where('classes.subject', '=', subject)
-      .join('users', 'classes.user_id', '=', 'users.id')
-      .select(['classes.*', 'users.*']);
+    try {
+      const classes = await db('classes')
+        .whereExists(function () {
+          this.select('class_schedule.*')
+            .from('class_schedule')
+            .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+            .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+            .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+            .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
+        })
+        .where('classes.subject', '=', subject)
+        .join('users', 'classes.user_id', '=', 'users.id')
+        .select(['classes.*', 'users.*']);
 
-    return res.json(classes);
+      return res.json(classes);
+    } catch (err) {
+      console.log(err);
+
+      return res.json({ error: 'An unexpected error occured.' });
+    }
   }
 
   async create(req: Request, res: Response) {
@@ -77,6 +83,7 @@ class ClassesController {
 
       return res.status(201).send();
     } catch (err) {
+      console.log(err);
       await trx.rollback();
 
       return res.status(400).json({ error: 'An unexpected error occurred.' });
